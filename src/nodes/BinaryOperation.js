823de4392625b450149b541764a0702815a1b4c8
Klaus Hott Vidal <klahott@gmail.com>
2019-06-20 20:15:09 +1200

    grouping and indenting exponentiation helps readability but adding spaces around it should be optional

--- src/nodes/BinaryOperation.js
+++ src/nodes/BinaryOperation.js
@@ -2,10 +2,10 @@
 const printers = require('../binary-operator-printers');
 
 const BinaryOperation = {
-  print: ({ node, path, print }) => {
+  print: ({ node, path, print, options }) => {
     for (let i = 0, len = printers.length; i < len; i += 1) {
       if (printers[i].match(node.operator))
-        return printers[i].print(node, path, print);
+        return printers[i].print(node, path, print, options);
     }
   }
 };

2c8a15aca0969cc8b51ca8672653b769188f7165
Klaus Hott Vidal <klahott@gmail.com>
2019-06-20 12:20:05 +1200

    no space and always indenting in exponentiation

--- src/binary-operator-printers/index.js
+++ src/binary-operator-printers/index.js
@@ -1,8 +1,17 @@
 const arithmetic = require('./arithmetic.js');
+const exponentiation = require('./exponentiation.js');
 const shift = require('./shift.js');
 const bit = require('./bit.js');
 const comparison = require('./comparison.js');
 const logical = require('./logical.js');
 const assignment = require('./assignment.js');
 
-module.exports = [arithmetic, shift, bit, comparison, logical, assignment];
+module.exports = [
+  arithmetic,
+  exponentiation,
+  shift,
+  bit,
+  comparison,
+  logical,
+  assignment
+];
